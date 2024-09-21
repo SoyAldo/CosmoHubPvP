0c01b727a2b94111f8b460dc539fac52b38e605e
SoyAldo <pereyraosvaldo@live.com.ar>
2024-09-21 19:58:59 -0300

    Initial commit

---
+++ src/main/java/com/soyaldo/cosmo/hubpvp/util/Messenger.java
@@ -0,0 +1,152 @@
+package com.soyaldo.luckypillarsinfection.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+public class Messenger {
+
+    private final JavaPlugin javaPlugin;
+    @Getter
+    @Setter
+    private String langType = "en_us";
+    @Getter
+    private Yaml lang;
+
+    public Messenger(JavaPlugin javaPlugin) {
+        this.javaPlugin = javaPlugin;
+    }
+
+    public Messenger(JavaPlugin javaPlugin, String langType) {
+        this.javaPlugin = javaPlugin;
+        this.langType = langType;
+    }
+
+    public void register() {
+        reload();
+    }
+
+    public void reload() {
+        InputStream inputStream = javaPlugin.getResource("lang/" + langType + ".yml");
+        if (inputStream == null) {
+            lang = new Yaml(javaPlugin, "lang", langType);
+            if (!lang.exist()) {
+                lang = new Yaml(javaPlugin, "lang", "en_us", javaPlugin.getResource("lang/en_us.yml"));
+            }
+        } else {
+            lang = new Yaml(javaPlugin, "lang", "en_us", inputStream);
+        }
+        lang.register();
+    }
+
+    public void send(String name, String path) {
+        send(name, path, new String[][]{});
+    }
+
+    public void send(String name, String path, String[][] replacements) {
+        if (name.equals("console")) {
+            CommandSender console = javaPlugin.getServer().getConsoleSender();
+            send(console, path, replacements);
+        } else {
+            Player player = javaPlugin.getServer().getPlayerExact(name);
+            if (player != null) send(player, path, replacements);
+        }
+    }
+
+    public void send(CommandSender commandSender, String path) {
+        send(commandSender, path, new String[][]{});
+    }
+
+    public void send(CommandSender commandSender, String path, String[][] replacements) {
+        Object message = lang.get(path, "&8[&4&l!&8] &cEl mensaje &f&n" + path + "&r &cno existe.");
+        String prefix = lang.getString("prefix", "");
+
+        replacements = Arrays.copyOf(replacements, replacements.length + 1);
+        replacements[replacements.length - 1] = new String[]{"%prefix%", prefix};
+
+        sendRaw(commandSender, message, replacements);
+    }
+
+    public void sendRaw(CommandSender commandSender, Object message) {
+        sendRaw(commandSender, message, new String[][]{});
+    }
+
+    public void sendRaw(CommandSender commandSender, Object message, String[][] replacements) {
+        // If the message is null.
+        if (message == null) {
+            // Return the method.
+            return;
+        }
+        // If the message is a String o List
+        if (message.getClass().getSimpleName().equals("String")) {
+            sendRaw(javaPlugin, commandSender, (String) message, replacements);
+        } else {
+            sendRaw(javaPlugin, commandSender, (List<String>) message, replacements);
+        }
+    }
+
+    public static void sendRaw(JavaPlugin javaPlugin, CommandSender commandSender, List<String> messages) {
+        // Use the sendRaw method with the empty replacements.
+        sendRaw(javaPlugin, commandSender, messages, new String[][]{});
+    }
+
+    public static void sendRaw(JavaPlugin javaPlugin, CommandSender commandSender, List<String> messages, String[][] replacements) {
+        // If the message is null.
+        if (messages == null) {
+            // Return the method.
+            return;
+        }
+        // Use the sendRaw in all list.
+        for (String message : messages) {
+            sendRaw(javaPlugin, commandSender, message, replacements);
+        }
+    }
+
+    public static void sendRaw(JavaPlugin javaPlugin, CommandSender commandSender, String message) {
+        // Use the sendRaw method with the empty replacements.
+        sendRaw(javaPlugin, commandSender, message, new String[][]{});
+    }
+
+    public static void sendRaw(JavaPlugin javaPlugin, CommandSender commandSender, String message, String[][] replacements) {
+        // If the message is null.
+        if (message == null) {
+            // Return the method.
+            return;
+        }
+
+        // Replace all replacements.
+        for (String[] replacement : replacements) {
+            message = message.replace(replacement[0], replacement[1]);
+        }
+
+        // PlaceholderAPI
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+                message = PlaceholderAPI.setPlaceholders(player, message);
+            }
+        }
+
+        // Generating the audience.
+        Audience audience = BukkitAudiences.create(javaPlugin).sender(commandSender);
+
+        // Generating the component.
+        Component component = MiniMessage.miniMessage().deserialize(message);
+
+        // Send the message.
+        audience.sendMessage(component);
+    }
+
+}
---
+++ src/main/java/com/soyaldo/cosmo/hubpvp/util/Yaml.java
@@ -0,0 +1,203 @@
+package com.soyaldo.luckypillarsinfection.utils;
+
+import com.google.common.io.ByteStreams;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+public class Yaml {
+
+    private final JavaPlugin javaPlugin;
+    private final String directory;
+    private final String name;
+    private FileConfiguration fileConfiguration;
+    private final InputStream defaultConfiguration;
+
+
+    public Yaml(JavaPlugin javaPlugin, String directory, String name, InputStream defaultConfiguration) {
+        this.javaPlugin = javaPlugin;
+        this.directory = javaPlugin.getDataFolder().getPath() + File.separator + directory;
+        this.name = name;
+        this.defaultConfiguration = defaultConfiguration;
+    }
+
+    public Yaml(JavaPlugin javaPlugin, String directory, String name) {
+        this.javaPlugin = javaPlugin;
+        this.directory = javaPlugin.getDataFolder().getPath() + File.separator + directory;
+        this.name = name;
+        defaultConfiguration = null;
+    }
+
+    public Yaml(JavaPlugin javaPlugin, String name, InputStream defaultConfiguration) {
+        this.javaPlugin = javaPlugin;
+        this.directory = javaPlugin.getDataFolder().getPath();
+        this.name = name;
+        this.defaultConfiguration = defaultConfiguration;
+    }
+
+    public Yaml(JavaPlugin javaPlugin, String name) {
+        this.javaPlugin = javaPlugin;
+        this.directory = javaPlugin.getDataFolder().getPath();
+        this.name = name;
+        defaultConfiguration = null;
+    }
+
+    public Yaml(String directory, String name, InputStream defaultConfiguration) {
+        this.javaPlugin = null;
+        this.directory = directory;
+        this.name = name;
+        this.defaultConfiguration = defaultConfiguration;
+    }
+
+    public Yaml(String directory, String name) {
+        this.javaPlugin = null;
+        this.directory = directory;
+        this.name = name;
+        defaultConfiguration = null;
+    }
+
+    public void register() {
+        if (fileConfiguration == null) {
+            reload();
+        }
+    }
+
+    public void reload() {
+        File directory = new File(this.directory);
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                javaPlugin.getLogger().info("Error: Fail in directory creation.");
+            }
+        }
+        File file = new File(directory, name + ".yml");
+        if (!file.exists()) {
+            try {
+                if (file.createNewFile()) {
+                    if (defaultConfiguration != null) {
+                        OutputStream outputStream = new FileOutputStream(file);
+                        ByteStreams.copy(defaultConfiguration, outputStream);
+                    }
+                }
+            } catch (IOException | IllegalArgumentException e) {
+                Bukkit.getLogger().log(Level.SEVERE, "Error: Local file failed in load.");
+            }
+        }
+        fileConfiguration = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void save() {
+        try {
+            File file = new File(directory, name + ".yml");
+            fileConfiguration.save(file);
+        } catch (IOException ignore) {
+            Bukkit.getLogger().log(Level.SEVERE, "Error: File do not saved.");
+        }
+    }
+
+    public boolean delete() {
+        // A File representing the directory is declared.
+        File directory = new File(this.directory);
+        // If the directory does not exist, return true.
+        if (!directory.exists()) {
+            return true;
+        }
+        // A File representing the file is declared.
+        File file = new File(directory, name + ".yml");
+        // If the file exists, what the delete method returns is returned.
+        if (file.exists()) {
+            return file.delete();
+        }
+        //If the file does not exist, return true.
+        return true;
+    }
+
+    public boolean exist() {
+        return new File(directory, name + ".yml").exists();
+    }
+
+    public Object get(String path) {
+        return fileConfiguration.get(path);
+    }
+
+    public Object get(String path, Object def) {
+        return fileConfiguration.get(path, def);
+    }
+
+    public FileConfiguration getFileConfiguration() {
+        if (fileConfiguration == null) reload();
+        return fileConfiguration;
+    }
+
+    public void set(String path, Object value) {
+        fileConfiguration.set(path, value);
+    }
+
+    public boolean contains(String... paths) {
+        for (String path : paths) {
+            if (!contains(path)) return false;
+        }
+        return true;
+    }
+
+    public boolean contains(String path) {
+        return fileConfiguration.contains(path);
+    }
+
+    public List<String> getKeys() {
+        return getKeys(false);
+    }
+
+    public List<String> getKeys(boolean deep) {
+        return new ArrayList<>(fileConfiguration.getKeys(deep));
+    }
+
+    public List<String> getKeys(String path) {
+        return getKeys(path, false);
+    }
+
+    public List<String> getKeys(String path, boolean deep) {
+        ConfigurationSection configurationSection = fileConfiguration.getConfigurationSection(path);
+        if (configurationSection == null) return new ArrayList<>();
+        return new ArrayList<>(configurationSection.getKeys(deep));
+    }
+
+    public String getString(String path) {
+        return fileConfiguration.getString(path);
+    }
+
+    public String getString(String path, String def) {
+        return fileConfiguration.getString(path, def);
+    }
+
+    public List<String> getStringList(String path) {
+        return fileConfiguration.getStringList(path);
+    }
+
+    public int getInt(String path) {
+        return fileConfiguration.getInt(path);
+    }
+
+    public int getInt(String path, int def) {
+        return fileConfiguration.getInt(path, def);
+    }
+
+    public boolean getBoolean(String path) {
+        return getBoolean(path, false);
+    }
+
+    public boolean getBoolean(String path, boolean def) {
+        return fileConfiguration.getBoolean(path, def);
+    }
+
+    public ConfigurationSection getSection(String path) {
+        return fileConfiguration.getConfigurationSection(path);
+    }
+
+}
